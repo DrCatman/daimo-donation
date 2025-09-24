c72b626f441e2acb8973f7bce308c6df9e3377cf
Catman <8276975+DrCatman@users.noreply.github.com>
2025-09-24 00:33:38 +0000

    set toToken for Optimism

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,7 @@ export const daimo = {
   // Base chain id
   toChain: 10,
   // Base USDC
-  toToken: "0x0000000000000000000000000000000000000000" as const,
-  //toToken: "0x833589fCD6eDb6E08f4c7C32D4f71B54bdA02913" as const,
+  toToken: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85" as const,
   // REQUIRED: destination address to receive funds
   toAddress: "0xc25132E7B939D689d402B11E3831b0b31a521Ab6",
   // Where to return after donation
