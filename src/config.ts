a2d078397cfa9242b173e5cdad869902237f9064
Catman <8276975+DrCatman@users.noreply.github.com>
2025-09-24 00:12:27 +0000

    0'd out toToken

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export const daimo = {
   // Base chain id
   toChain: 10,
   // Base USDC
+  toToken: "0x0000000000000000000000000000000000000000" as const,
   //toToken: "0x833589fCD6eDb6E08f4c7C32D4f71B54bdA02913" as const,
   // REQUIRED: destination address to receive funds
   toAddress: "0xc25132E7B939D689d402B11E3831b0b31a521Ab6",
